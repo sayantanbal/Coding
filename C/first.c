25fd14c652f4e7492d58b9ef8a586ba96f33241a
wolverine013 <balsayantan3@gmail.com>
2023-10-15 20:30:39 +0530

    lgtm

--- C/first.c
+++ C/first.c
@@ -1,8 +1,17 @@
+<<<<<<< HEAD
 #include <stdio.h>
 int main()
 {
     float a = 5.65;
     printf("%.35f", a);
+=======
+#include<stdio.h>
+int main(){
+    char a = '#';
+    printf("\n\n%d\n\n",a);
+return 0;
+}
+>>>>>>> 429a109 (lgtm)
 
     // char b = 'Z';
     // printf("\n \n %d \n \n", b);
---
+++ JavaScript/01_basics.js
@@ -0,0 +1,26 @@
+"use strict"; // use all your JS code as newer version even though modern day engine execute as strict code
+// alert(3+3) we are using NodeJS not browser.
+//console.log(3); // Right as we're using semicolon to end the line.
+//but 
+/* console.log(3) console.log(33) --> not correct as removing a semicolon means line continues. */
+
+// must maintain code readability
+
+let name = "Sayantan"
+let number = 10
+let isLoggedIn = false
+
+// number = 2^53
+// bigint 
+// string => "" or ''
+// boolean => true or false
+// null --> is a standalone value! It's a type. It's a special type 
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof(undefined)); // undefined
+console.log(typeof(null)); //object
