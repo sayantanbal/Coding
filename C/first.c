a0a2533074324ae20b3e6f05c3b25d54f105d4d5
wolverine013 <balsayantan3@gmail.com>
2023-10-15 16:03:55 +0530

    lgtm

--- C/first.c
+++ C/first.c
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int main()
 {
-    float a = 5.65;
-    printf("%.35f", a);
-=======
-#include<stdio.h>
-int main(){
     char a = '#';
-    printf("\n\n%d\n\n",a);
-return 0;
-}
->>>>>>> 429a109 (lgtm)
+    printf("\n\n%d\n\n", a);
 
     // char b = 'Z';
     // printf("\n \n %d \n \n", b);
