be10ae118a1ae9595c06e11e6d524075740986ca
wolverine013 <balsayantan3@gmail.com>
2023-10-15 09:53:27 +0530

    lgtm

--- C/first.c
+++ C/first.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    float a = 5.75;
-    printf("%.15f", a);
+    // float a = 5.75;
+    // printf("%.15f", a);
+
+    char b = 'A';
+    printf("\n \n %d \n \n", b);
+
     return 0;
 }
