2cb70ab3de75f9c8144a135471cb75c3d66111d4
wolverine013 <balsayantan3@gmail.com>
2023-10-16 10:28:05 +0530

    lgtm

--- JavaScript/02_conversionOperations.js
+++ JavaScript/02_conversionOperations.js
@@ -24,7 +24,65 @@
 // console.log(booleanisLoggedIn);
 
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-// console.log(stringNumber);  
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// // console.log(stringNumber);  
+// console.log(typeof stringNumber);
+
+
+
+
+                /**************OPERATIONS***************/
+
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2); 
+// console.log(3**5); // ** means 3 to the power 5
+// console.log(2%2);
+
+
+// let str1 = "hello";
+// let str2 = "Sayantan";
+
+// let str3 = str1+str2;
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log(1+2+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+
+
+// console.log(true); // gives "true" as a output
+// console.log(+true); // means (+true);converts to a number and gives 1. 
+// console.log(true+);// isn't a correct syntax
+// console.log(+""); // means (+false); converts to a number and gives 0. 
+// console.log(+"    "); // means (+true); converts to a number and gives 0.
+
+
+
+
+// let abc = (+true);
+// let abc = (+false);
+// console.log(typeof abc);
+
+
+
+gameCounter = 100;
+
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+console.log(gameCounter++);
+console.log(++gameCounter);
+console.log(--gameCounter);
+console.log(gameCounter--);
+// console.log(++gameCounter--);
+// console.log(--gameCounter++); both are wrong syntax.
