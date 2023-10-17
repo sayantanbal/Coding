54b9bcef8871a316b0a64bbab556d4ad3e890b6e
wolverine013 <balsayantan3@gmail.com>
2023-10-17 11:32:16 +0530

    lgtm

---
+++ JavaScript/05_nums&maths.js
@@ -0,0 +1,36 @@
+// const score = 400
+
+// const balance = new Number(100000)
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString.length); // toString with out parenthesis only converts 1st letter to String.
+
+// console.log(balance.toString().length); // toString with parenthesis converts the whole to String.
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 122.55696
+// console.log(otherNumber.toPrecision(4)); // the argument means how much value to be focused on.
+
+// const hundreds = 100002310;
+// console.log(hundreds.toLocaleString('en-IN')); // converts to a string with commas for lagre numbers. argument takes how to show te value
+
+/************************************ maths *********************************/
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+//  console.log(Math.round(-4.558));
+// console.log(Math.ceil(-4.558));
+// console.log(Math.floor(-4.558));
+
+// console.log(Math.min(-4, 56, 7, 8));
+// console.log(Math.max(-4, 56, 7, 8));
+
+
+// console.log(Math.random()); 
+
+const min = 10
+const max = 20
+let x = Math.floor(Math.random() * (max - min +1 ) + min)
+console.log(x);
