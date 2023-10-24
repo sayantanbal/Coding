010151182fdc4a1b3b17d72f6caf3a5fb615b0c3
wolverine013 <balsayantan3@gmail.com>
2023-10-24 12:25:38 +0530

    lgtm

--- JavaScript/JS Practice/practice5.js
+++ JavaScript/JS Practice/practice5.js
@@ -1,2 +1,15 @@
 // Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.
 
+function isSunday() {
+  for (let index = 2014; index <= 2050; index++) {
+    let newDate = new Date(index, 0, 1);
+    if (newDate.getDay() === 0) {
+      // const newDate = firstJansIsSunday[index];
+      console.log("1st Jan is Sunday of the the year :" + index);
+      //   return firstJansIsSunday
+    }
+  }
+}
+
+// console.log(isSunday())
+isSunday();
