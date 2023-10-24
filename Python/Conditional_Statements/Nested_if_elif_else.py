c6116338041db9c59407cab20428e34c687098c5
wolverine013 <balsayantan3@gmail.com>
2023-10-24 19:20:36 +0530

    lgtm

--- Python/Conditional_Statements/Nested_if_elif_else.py
+++ Python/Conditional_Statements/Nested_if_elif_else.py
@@ -6,6 +6,8 @@ elif(num>0):
         print("Number is in between 1-10!")
     elif(num>10 and num<=20):
         print("Number is in between 11-20")
+    # elif(num>10 and num<=20):
+    #     print("Number is in between 11-20") #once condition matched other conditions are overlooked even though there are same conditions
     else:
         print("Number is greater than 20")
 else:
--- Python/Strings/String_Slicing.py
+++ Python/Strings/String_Slicing.py
@@ -1,13 +1,19 @@
 names = "Tuhin, subham, Anushka"
-print(names[0:5]) # we will get first 5 characters
+# print(names[0:5]) # we will get first 5 characters
 # It will run to [5-1] index
 # we can specify the indexes. By default the starting index is 0 and ending index is->
 # len(string_name)
-print(len(names))
+# print(len(names))
 fruit = "Mango"
-print(fruit[0:-3])
-print(fruit[0:-3])
-print("new")
+# print(fruit[0:-3])
+# print(fruit[0:-3])
+# print("new")
 # print(fruit[0:len(fruit)-3]) --> the meaning of the upper line
-print(fruit[-1:-3])
-print(fruit[-3:-1])
+print(fruit[-1:-3]) # not valid so gives an empty space. even though doesn't throw an error
+
+abc = "harryO"
+# print(abc[-4:-2])
+print(abc.capitalize())
+print(len(abc))
+print(abc.center(20))
+print(len(abc.center(20)))
