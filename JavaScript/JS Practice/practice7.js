ccad2f7b26c53daa9d809e27ee109ea0c51cb0a5
wolverine013 <balsayantan3@gmail.com>
2023-10-24 13:38:52 +0530

    practice on Dates

--- JavaScript/JS Practice/practice7.js
+++ JavaScript/JS Practice/practice7.js
@@ -1,1 +1,18 @@
-// Write a JavaScript program to calculate the days left before Christmas.  
+// Write a JavaScript program to calculate the days left before Christmas.
+
+let currDate = new Date();
+let christmasYear = currDate.getFullYear();
+
+function remDaysCalculator(anyDate) {
+  if (currDate.getMonth() === 11 && currDate.getDate() > 25) {
+    christmasYear = christmasYear + 1;
+  }
+  let christmasDate = new Date(christmasYear, 11, 25);
+  let msInaDay = 24 * 60 * 60 * 1000;
+
+  let remainingDays = Math.ceil((christmasDate.getTime() - currDate.getTime()) / msInaDay);
+
+  console.log(`${remainingDays} Days more before Christmas`);
+}
+
+remDaysCalculator(currDate);
