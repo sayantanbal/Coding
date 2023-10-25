0e0a86b20b4db2eee7888546cb59c673e50320f5
wolverine013 <balsayantan3@gmail.com>
2023-10-25 21:52:27 +0530

    lgtm

--- Python/06_Functions/02_func.py
+++ Python/06_Functions/02_func.py
@@ -19,7 +19,7 @@ c = average(5, 6, 7, 1)
 print(c)
 
 
-# def name(**name):
+# def name(**name): # ** is used when we make a dictionary
 #   # print(type(name))
 #   print("Hello,", name["fname"], name["mname"], name["lname"])
 
