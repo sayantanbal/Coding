17cacb8fbc91eeec64b98c791e301e1c0442367e
wolverine013 <balsayantan3@gmail.com>
2023-10-26 22:23:57 +0530

    practice

--- JavaScript/JS Practice/practice10.js
+++ JavaScript/JS Practice/practice10.js
@@ -1,1 +1,11 @@
 //Write a JavaScript program to get the difference between a given number and 13, if the number is broader than 13 return double the absolute difference.
+const prompt = require("prompt-sync")({ sigint: true });
+let userInput = Number(prompt("Enter a number : "));
+
+if (userInput <= 13) {
+  console.log(`The diff b/w user given number and 13 is ${userInput - 13}`);
+} else {
+  console.log(
+    `double the absolute difference is ${Math.abs(userInput - 13) * 2}`
+  );
+}
---
+++ JavaScript/JS Practice/practice11.js
@@ -0,0 +1,1 @@
+//  21. Write a JavaScript program to create another string by adding "Py" in front of a given string. If the given string begins with "Py" return the original string.
