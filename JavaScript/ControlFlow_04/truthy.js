241ea07102708ac487b896f0327796f5739e879a
wolverine013 <balsayantan3@gmail.com>
2023-10-26 11:29:23 +0530

    lgtm

--- JavaScript/ControlFlow_04/truthy.js
+++ JavaScript/ControlFlow_04/truthy.js
@@ -29,7 +29,7 @@ let val1;
 // val1 = 5 ?? 10
 // val1 = null ?? 10
 // val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+val1 = null ?? 10 ?? 20 // returns the value that is assigned first here 10.
 
 
 
