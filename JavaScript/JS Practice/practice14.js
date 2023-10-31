ac174961739aed173baf12f9f7b5cc31f024163b
wolverine013 <balsayantan3@gmail.com>
2023-10-31 09:20:25 +0530

    practice

--- JavaScript/JS Practice/practice14.js
+++ JavaScript/JS Practice/practice14.js
@@ -1,16 +1,20 @@
-//42. Write a JavaScript program to check whether three given numbers are increasing in strict or in soft mode.  
+//42. Write a JavaScript program to check whether three given numbers are increasing in strict or in soft mode.
 //Note: Strict mode -> 10, 15, 31 : Soft mode -> 24, 22, 31 or 22, 22, 31
 
-function isStrictorSoftModeInc (num1,num2,num3){
-    if (num3>num2 && num3>num1) {
-        if (num2>num1) {
-            console.log("Three numbers are strictly inc.");
-        }
-        else
-        console.log("Three numbers are softly inc.");
-        
-    }
+function isStrictorSoftModeInc(num1, num2, num3) {
+  if (num1 < num2 && num2 < num3)
+    console.log("Three numbers are strictly inc.");
+  else if ((num1 >= num2 && num2 < num3) || (num1 < num2 && num2 <= num3))
+    console.log("Three numbers are softly inc.");
+  else if (num1 === num2 && num2 === num3)
+    console.log("Three numbers are same");
+  else console.log("Three numbers are not inc.");
 }
 
+const prompt = require("prompt-sync")({ sigint: true });
+console.log("You have to enter 3 numbers");
+let userInput1 = Number(prompt("Enter 1st number "));
+let userInput2 = Number(prompt("Enter 2nd number "));
+let userInput3 = Number(prompt("Enter 3rd number "));
 
-
+isStrictorSoftModeInc(userInput1, userInput2, userInput3);
---
+++ JavaScript/JS Practice/practice15.js
@@ -0,0 +1,1 @@
+// 53. Write a JavaScript program to check whether the characters a and b are separated by exactly 3 places anywhere (at least once) in a given string.  
