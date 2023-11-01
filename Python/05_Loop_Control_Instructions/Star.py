326c8224fc72172eb35688db007667d68fa4814d
wolverine013 <balsayantan3@gmail.com>
2023-11-01 08:46:09 +0530

    var update

--- Python/05_Loop_Control_Instructions/Star.py
+++ Python/05_Loop_Control_Instructions/Star.py
@@ -1,6 +1,6 @@
 n = int(input("Enter the number of rows:"))
-for k in range(1, n+1):
-    for i in range(k):
+for startloop_Iterator in range(1, n+1):
+    for insideloop_Iterator in range(startloop_Iterator):
         print("*", end='')
     print()
 
