17883521b665ea64a07c2509c8fcf51ef0b35848
wolverine013 <balsayantan3@gmail.com>
2023-11-08 12:48:18 +0530

    lgtm

--- C++/abc.cpp
+++ C++/abc.cpp
@@ -1,21 +1,74 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
-#include<iostream>
-#include<string>
 using namespace std;
-int main()
-{
-    // char a = 'a',b = 'b' ;
-
-
-    string str1 = "Hello";
-    char a = str1[0];
-    char b = str1[4];
-    cout << a << b << endl;
-
-    a = a^b;
-    b = a^b;
-    a = a^b;
-    cout << a << b << endl;
-    
-return 0;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int  a , b;
+    cin >> a >> b;
+    for (int n = a; n<=b; n++) {
+    if (n<=9) {
+    switch (n)
+    {
+    case 1:
+        cout << "one\n";
+        break;
+    case 2:
+        cout << "two\n";
+        break;
+    case 3:
+        cout << "three\n";
+        break;
+    case 4:
+        cout << "four\n";
+        break;
+    case 5:
+        cout << "five\n";
+        break;
+    case 6:
+        cout << "six\n";
+        break;
+    case 7:
+        cout << "seven\n";
+        break;
+    case 8:
+        cout << "eight\n";
+        break;
+    case 9:
+        cout << "nine\n";
+        break;
+    case 0:
+        cout << "zero\n";
+        break;
+    }
+    }
+    else if(n > 9){
+        if (n % 2 == 0) {
+            cout << "even\n";
+        }
+        else
+        cout << "odd";
+    }
+    }
+    return 0;
 }
