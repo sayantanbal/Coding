b8bc4b0d71c13d3027adf043125a92b7c09b959d
wolverine013 <balsayantan3@gmail.com>
2023-11-12 22:06:11 +0530

    lgtm

--- JavaScript/try.js
+++ JavaScript/try.js
@@ -1,5 +1,11 @@
-console.log(new Boolean(false) == false)
+// console.log(new Boolean(false) == false)
 
-let x = new Boolean(false)
-console.log(typeof(x));
-console.log(typeof(false));
+// let x = new Boolean(false)
+// console.log(typeof(x));
+// console.log(typeof(false));
+
+const a = <div> {age} </div>;
+const b = <div> {age} </div>;
+
+
+console.log(typeof (a), typeof(b));
