942688db55773c2f01d8faa5374bc20372f2cccb
wolverine013 <balsayantan3@gmail.com>
2023-11-20 10:39:29 +0530

    lgtm

--- C/try.c
+++ C/try.c
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
-int main()
-{
-
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    char ch;
-    scanf("%c", &ch);
-    char s[100];
-    scanf("%s", s);
-    char sen[100];
-    scanf("\n");
-    scanf("%[^\n]%*c", sen);
-    
-
-    printf("%c\n", ch);
-    printf("%s\n", s);
-    // printf("%s", sen);
-    puts(sen);
-    return 0;
-}
+<<<<<<< HEA
 =======
 #include<stdio.h>
 #include<math.h>
