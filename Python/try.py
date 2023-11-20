cbfe51b8ff94c95dfb5b524eca616043e2a17a27
wolverine013 <balsayantan3@gmail.com>
2023-11-20 10:23:57 +0530

    lgtm

--- Python/try.py
+++ Python/try.py
@@ -1,15 +1,30 @@
-if __name__ == '__main__':
-    n = int(input())
+# if __name__ == '__main__':
+#     n = int(input())
     
-    student_marks = {}
-    for _ in range(n):
-        name, *line = input().split()
-        scores = list(map(float, line))
-        student_marks[name] = scores
-    # query_name = input()
+#     student_marks = {}
+#     for _ in range(n):
+#         name, *line = input().split()
+#         scores = list(map(float, line))
+#         student_marks[name] = scores
+#     # query_name = input()
     
-    # result = sum(student_marks[query_name])/len(student_marks[query_name])
+#     # result = sum(student_marks[query_name])/len(student_marks[query_name])
 
 
-    # formatted_result = '{:.2f}'.format(result)
-    # print(formatted_result)
+#     # formatted_result = '{:.2f}'.format(result)
+#     # print(formatted_result)
+
+
+
+
+from pytube import Playlist
+playlist = Playlist("https://youtube.com/playlist?list=PLu0W_9lII9aikXkRE0WxDt1vozo3hnmtR&si=P_wRmu3uoL4mVBO0")
+
+print(f"tottal videos are {len(playlist.video_urls)}")
+
+for video in playlist.videos :
+    print(video.streams.filter(file_extension="mp4"))
+    # video.streams.first().download("D:\PHP Harry")
+
+
+# from pytube import YouTube
