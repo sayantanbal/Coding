36141a01e2af6a75bf119118c930bd366ff438a7
wolverine013 <balsayantan3@gmail.com>
2023-11-26 13:52:46 +0530

    lgtm

---
+++ Python/21_OOP/12_dir__dict__help.py
@@ -0,0 +1,28 @@
+x = [1, 2, 3]
+print(dir(x))
+# ['__add__', '__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p = Person("John", 30)
+p.__dict__
+
+
+# help(str)
+# # Help on class str in module builtins:
+
+# class str(object):
+#    str(object='') -> str
+#    str(bytes_or_buffer[, encoding[, errors]]) -> str
+
+#  |  Create a new string object from the given object. If encoding or
+#  |  errors is specified, then the object must expose a data buffer
+#  |  that will be decoded using the given encoding and error handler.
+#  |  Otherwise, returns the result of object.__str__() (if defined)
+#  |  or repr(object).
+#  |  encoding defaults to sys.getdefaultencoding().
+#  |  errors defaults to 'strict'.
