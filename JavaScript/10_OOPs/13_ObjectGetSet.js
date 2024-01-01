b2ea5276aa9f15037f378e8c163dbde6384e0725
sayantanbal <balsayantan3>
2024-01-01 10:20:33 +0530

    Add Promises, Object Get/Set, API Request, and Closure files

--- JavaScript/10_OOPs/13_ObjectGetSet.js
+++ JavaScript/10_OOPs/13_ObjectGetSet.js
@@ -1,26 +1,16 @@
-function User(email, password){
-    this._email = email;
-    this._password = password
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
 
-    Object.defineProperty(this, 'email', {
-        get: function(){
-            return this._email.toUpperCase()
-        },
-        set: function(value){
-            this._email = value
-        }
-    })
-    Object.defineProperty(this, 'password', {
-        get: function(){
-            return this._password.toUpperCase()
-        },
-        set: function(value){
-            this._password = value
-        }
-    })
 
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
 }
 
-const chai = new User("chai@chai.com", "chai")
-
-console.log(chai.email);
+const tea = Object.create(User) // object.create() creates a new object with the specified prototype object and properties. it is a factory method.
+console.log(tea.email);
