9f8c40f98733a60b1e04d38ad9631a6cf67fddf0
sayantanbal <balsayantan3>
2024-01-23 03:24:42 +0530

    Fix code formatting and remove commented out code

--- C/try.c
+++ C/try.c
@@ -8,53 +8,45 @@
 //     printf("a = % d, b = % d, c = % d, d = % d\n ", a, b, c, d);
 
 //     // printf("%d\n", 0||1);
-    
+
 //     d= a-- && --b;
 //     printf("a = % d, b = % d, c = % d, d = % d\n ", a, b, c, d);
 //     return 0;
 // }
 
-
-
-//1. Sum of first n natural numbers
-
+// 1. Sum of first n natural numbers
 
 // #include<stdio.h>
 
 // void main(){
 //     printf("Enter how many terms you want? ");
-//     int n, sum = 0 ; 
+//     int n, sum = 0 ;
 //     scanf("%d", &n);
 //     for (int i = 0; i <= n; i++)
 //     {
-//         sum += i; 
+//         sum += i;
 //     }
 //     printf("Sum of Natural Numbers upto %d is %d", n, sum);
 
-    
 // }
 
-
-
-//2. Find the average of the first N natural numbers
+// 2. Find the average of the first N natural numbers
 
 // #include<stdio.h>
 // void main (){
 //     printf("Enter how many terms you want? ");
 //     int n, sum = 0;
-//     float avg = 0 ; 
+//     float avg = 0 ;
 //     scanf("%d", &n);
 //     for (int i = 1; i <= n; i++)
 //     {
-//         sum += i; 
+//         sum += i;
 //     }
-//     avg = (float)sum/n; // you can typecast here or just simply take all numbers including inputs in float. we're doing this for the possibility of fractional outcomes. 
+//     avg = (float)sum/n; // you can typecast here or just simply take all numbers including inputs in float. we're doing this for the possibility of fractional outcomes.
 //     printf("%.3f", avg);
 // }
 
-
 // 3.  Find the factorial of a number N.
-
 
 // #include<stdio.h>
 // void main(){
@@ -70,19 +62,133 @@
 
 // 4. Given 3 numbers, find their maximum and minimum
 
-#include<stdio.h>
+// #include<stdio.h>
 
-void main(){
-    float a,b,c;
-    printf("Enter three numbers -> ");
-    scanf("%f%f%f", &a,&b,&c);
-    if (a == b && b == c)
-        printf("given numbers all are equal");
-    else if (a>b && a>c)
-        printf("%d is maximum", a);
-    else if (b>a && b>c)
-        printf("%d is maximum", b);
-    else
-        printf("%d is minimum", c);
-    
+// void main(){
+//     float a,b,c;
+//     printf("Enter three numbers -> ");
+//     scanf("%f%f%f", &a,&b,&c);
+//     if (a == b && b == c)
+//         printf("given numbers all are equal");
+//     else if (a>b && a>c)
+//         printf("%d is maximum", a);
+//     else if (b>a && b>c)
+//         printf("%d is maximum", b);
+//     else
+//         printf("%d is minimum", c);
+
+// }
+
+// #include <stdio.h>
+// void main()
+// {
+//     int i;
+//     for (i = 0; i = 3; i++){
+//         printf("fdasf");
+//         if (i == 2)
+//         {
+//             printf("Inside loop %d\n", i);
+//             break;
+//         }
+//     }
+//     printf("outside loop %d", i);
+// }
+
+#include <stdio.h>
+void main()
+{
+    // int x = 10 , y = 10 , z = 10;
+    // if (x > y && x > z)
+    //     printf("x is greater");
+    // else if (y > x && y > z)
+    //     printf("y is greater");
+    // else
+    //     printf("z is greater");
+
+    // (x > y && x > z) ? printf("x is greater") : (y > x && y > z) ? printf("y is greater") : printf("z is greater");
+
+    // int x = 58, y = 28;
+    // float z = (float)x / (float)y;
+    // float z1 = x/y;
+    // printf("%0.2f %0.2f \n", z, z1);
+
+    // int i = 0;
+    // while (i < 10)
+    // {
+    //     i++;
+    //     if (i == 5)
+    //         continue;
+    //     printf("%d\n", i);
+    // }
+
+    // int i = 0;
+    // while (i < 10)
+    // {
+    //     i++;
+    //     if (i == 5)
+    //         break;
+    //     printf("%d\n", i);
+    // }
+
+    // int a = 8, b = 7, temp;
+    // printf("a = % d, b = % d\n", a, b);
+    // temp = a, a = b, b = temp;
+    // printf("a = % d, b = % d\n", a, b);
+
+    // int n, i, flag = 0;
+    // printf("Enter a positive integer: ");
+    // scanf("%d", &n);
+    // for (i = 2; i <= n / 2; ++i)
+    // {
+    //     // condition for nonprime number
+    //     if (n % i == 0)
+    //     {
+    //         flag = 1;
+    //         break;
+    //     }
+    // }
+    // if (n == 1)
+    // {
+    //     printf("1 is neither prime nor composite.");
+    // }
+    // else
+    // {
+    //     if (flag == 0)
+    //         printf("%d is a prime number.", n);
+    //     else
+    //         printf("%d is not a prime number.", n);
+    // }
+
+    // int n = 925, reversedNumber = 0, remainder;
+    // while (n != 0)
+    // {
+    //     remainder = n % 10;
+    //     reversedNumber = reversedNumber * 10 + remainder;
+    //     n = n / 10;
+    // }
+    // printf("Reversed Number = %d", reversedNumber);
+
+    int n = 54321;
+    int reversedNumber = 0, remainder;
+    while (n != 0)
+    {
+        remainder = n % 10;
+        if (remainder % 2 != 0)
+        {
+            reversedNumber = reversedNumber * 10 + remainder;
+        }
+        n = n / 10;
+    }
+    printf("Reversed Number = %d \n", reversedNumber);
+
+    // now we have to reverse the reversed number
+
+    int reversedNumber2 = 0, remainder2;
+    while (reversedNumber != 0)
+    {
+        remainder2 = reversedNumber % 10;
+        reversedNumber2 = reversedNumber2 * 10 + remainder2;
+        reversedNumber = reversedNumber / 10;
+    }
+    printf("Reversed Number = %d", reversedNumber2);
 }
--- JavaScript/try.js
+++ JavaScript/try.js
@@ -7,15 +7,10 @@
 // const a = <div> {age} </div>;
 // const b = <div> {age} </div>;
 
-
 // console.log(typeof (a), typeof(b));
-
-
-
 
 // console.log(parseInt('a444')); //44 is given beacuse it is a valid number but aa is not a valid number so it will stop there.
 // console.log(parseInt('44a4a')); //44 is given beacuse it is a valid number but aa is not a valid number so it will stop there.
-
 
 /**
 //  * @return {Function}
@@ -35,21 +30,24 @@
 // counter() // 11
 // counter() // 12
 
-
-var expect = function(val) {
-    return {
-        toBe: (val2) => {
-            if (val !== val2) throw new Error("Not Equal");
-            else return true;
-        },
-        notToBe: (val2) => {
-            if (val === val2) throw new Error("Equal");
-            else return true;
-        }
-    }
-};
+// var expect = function(val) {
+//     return {
+//         toBe: (val2) => {
+//             if (val !== val2) throw new Error("Not Equal");
+//             else return true;
+//         },
+//         notToBe: (val2) => {
+//             if (val === val2) throw new Error("Equal");
+//             else return true;
+//         }
+//     }
+// };
 
 /* For example, when expect(5).toBe(4) is called,
    val is the expect parameter (so val equals 5),
    val2 is the toBe parameter (so val2 equals 4).
    Since val !== val2, aka 5 != 4, we throw error "Not Equal". */
+
+console.log(016 == "016");
+console.log(018 == "018");
+console.log(typeof 018, typeof "018");
