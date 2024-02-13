6cd68160139257d9d08a5d8f0a73d83ca8c4be7c
sayantanbal <balsayantan3>
2024-02-13 12:40:25 +0530

    Fix data type formatting and add new file

---
+++ C/meow.c
@@ -0,0 +1,47 @@
+// #include <stdio.h>
+// void main()
+// {
+//     printf("Meow, World!\n");
+//     // return 0;
+//     int a ;
+//     printf("Enter a number: ");
+//     scanf("%d", &a);
+//     printf("%d", a);
+// }
+
+// Data Types
+// int, float, double, char, void
+// int: 4 bytes
+// float: 4 bytes
+// double: 8 bytes
+// char: 1 byte
+// void: no data type
+
+/*
+If else statement
+syntax:
+    if (condition)
+    {
+        // code
+    }
+    else
+    {
+        // code
+    }
+*/
+
+// #include <stdio.h>
+// int main()
+// {
+//     int number = 0;
+//     printf("Enter a number:");
+//     scanf("%d", &number);
+//     if (number % 2 == 0)
+//     {
+//         printf("%d is even number", number);
+//     }
+//     return 0;
+// }
+
+
+
