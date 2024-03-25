350188cfb11a25315c92272668a84b1645517990
sayantanbal <balsayantan3>
2024-03-25 09:42:12 +0530

    Add basic C program and fix error handling in Python script

---
+++ C/Basic_Programs/basic.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int v;
+    printf("Enter a number\n");
+    scanf("%d", v);
+    printf("V is : %d", v);
+    return 0;
+}
--- Python/Python_Hitesh/09_error_handling/youtube_manager.py
+++ Python/Python_Hitesh/09_error_handling/youtube_manager.py
@@ -30,25 +30,25 @@ def add_video(videos):
 
 def update_video(videos):
     list_all_videos(videos)
-    index = int(input("Enter the video number to update"))
+    index = int(input("Enter the video number to update: "))
     if 1 <= index <= len(videos):
-        name = input("Enter the new video name")
-        time = input("Enter the new video time")
+        name = input("Enter the new video name: ")
+        time = input("Enter the new video time: ")
         videos[index-1] = {'name':name, 'time': time}
         save_data_helper(videos)
     else:
-        print("Invalid index selected")
+        print("Invalid index selected. ")
 
 
 def delete_video(videos):
     list_all_videos(videos)
-    index = int(input("Enter the video number to be deleted"))
+    index = int(input("Enter the video number to be deleted: "))
     
     if 1<= index <= len(videos):
         del videos[index-1]
         save_data_helper(videos)
     else:
-        print("Invalid video index selected")
+        print("Invalid video index selected. ")
 
 
 def main():
@@ -75,7 +75,7 @@ def main():
             case '5':
                 break
             case _:
-                print("Invalid Choice")
+                print("Invalid Choice. ")
 
 if __name__ ==  "__main__":
     main() 
