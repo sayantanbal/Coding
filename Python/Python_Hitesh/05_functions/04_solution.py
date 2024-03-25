e73e6028a7b074821ddca969a4467bb2bda16da7
sayantanbal <balsayantan3>
2024-03-25 09:43:17 +0530

    Fix return statement in circle_stats function

--- Python/Python_Hitesh/05_functions/04_solution.py
+++ Python/Python_Hitesh/05_functions/04_solution.py
@@ -3,7 +3,7 @@ import math
 def circle_stats(radius):
     area = math.pi * radius ** 2
     circumference = 2 * math.pi * radius
-    return area, circumference # return multiple values.
+    return (area, circumference) # return multiple values.
 
 a, c = circle_stats(3) # unpack the returned values. you can take multiple variables to unpack multiple values.
 
