52d6f9c2aaf2bd6c22d5f36e9cd6037925820910
sayantanbal <balsayantan546@gmail.com>
2025-01-11 22:56:20 +0530

    Refactor print examples in BasicsOfPython and add new error handling example; create initial Jupyter Notebook files

--- Python/Data Science & ML/01_BasicsOfPython/01_Datatypes.py
+++ Python/Data Science & ML/01_BasicsOfPython/01_Datatypes.py
@@ -1,9 +1,12 @@
-# print has many extra arguments like end, sep, etc. which can be used to customize the output
+# # print has many extra arguments like end, sep, etc. which can be used to customize the output
 
-print(5, 6, 7, 8, sep=" ", end="!\n")
-# sep --->> separator is used to specify what to print between the values in the line
-# end --->> end parameter is used to specify what to print at the end of the line
+# print(5, 6, 7, 8, sep=" ", end="!\n")
+# # sep --->> separator is used to specify what to print between the values in the line
+# # end --->> end parameter is used to specify what to print at the end of the line
 
-# some more examples of sep and end
-print(5, 6, 7, 8, sep=",", end="!\n")
-print(5, 6, 7, 8, sep="*", end="!\n")
+# # some more examples of sep and end
+# print(5, 6, 7, 8, sep=",", end="!\n")
+# print(5, 6, 7, 8, sep="*", end="!\n")
+
+for i in range(100000000):
+    print()
---
+++ Python/Data Science & ML/05_ErrorHandelling/01_error.py
@@ -0,0 +1,2 @@
+li = [1, 23, "hello", 1]
+print(type(li))
