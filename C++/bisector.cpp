c57d2663c6f10bee0ee23c778bacea15f7719fbe
sayantanbal <balsayantan546@gmail.com>
2025-02-22 20:29:17 +0530

    Add C++ bisection method implementation and configuration settings

---
+++ C++/bisector.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+#define EPSILON 0.01
+
+
+double func(double x)
+{
+    return x * x * x * x * x - 2;
+}
+
+// Prints root of func(x) with error of EPSILON
+void bisection(double a, double b)
+{
+    if (func(a) * func(b) >= 0)
+    {
+        cout << "You have not assumed right a and b\n";
+        return;
+    }
+
+    double c = a;
+    while ((b - a) >= EPSILON)
+    {
+        c = (a + b) / 2;
+
+        if (func(c) == 0.0)
+            break;
+
+        else if (func(c) * func(a) < 0)
+            b = c;
+        else
+            a = c;
+    }
+    cout << "The value of root is : " << c;
+}
+
+
+int main()
+{
+    double a = 1, b = 2;
+    bisection(a, b);
+    return 0;
+}
