77fd53dc0326cb3a332c82e9a29b091bd5c21862
sayantanbal <balsayantan546@gmail.com>
2025-03-04 16:11:23 +0530

    Remove unused file handling code and clean up output file

--- C++/DSA/problems/01_STL.cpp
+++ C++/DSA/problems/01_STL.cpp
@@ -2,13 +2,10 @@
 using namespace std;
 int main()
 {
-
-
-
-#ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+// freopen("output.txt", "w", stdout);
+// freopen("input.txt", "r", stdin);
+// #endif
 
 
 
